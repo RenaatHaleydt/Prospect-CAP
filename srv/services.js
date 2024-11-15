bb3d02bc96cf186e81ab4c13f858b9d2ce5fe698
Renaat Haleydt <p150480@ap.be>
2024-11-15 09:12:48 +0000

    Connecting to Northwind destination defined in SAP BTP

--- srv/services.js
+++ srv/services.js
@@ -1,5 +1,32 @@
+const httpclient = require("@sap-cloud-sdk/http-client");
+
 module.exports = async function (srv) {
     srv.before("CREATE", "Prospects", async (oReq) => {
       console.log("let's try")
     });
+
+    srv.before("READ", "Prospects", async (oReq) => {
+      let products = await getFirstProductFromNorthwind()
+    });
+
+}
+
+async function getFirstProductFromNorthwind() {
+	let oResponse = await httpclient.executeHttpRequest({
+        destinationName: 'northwind'
+    }, {
+        method: 'GET',
+        url: '/Products',
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        params: {
+          top: 1,
+          format: 'json'
+        },
+        parameterEncoder: httpclient.encodeAllParameters
+    }).catch(oError => {
+		  console.log(`Something went wrong connecting to the Northwind destination`)
+    });
+	return oResponse.data;
 }
