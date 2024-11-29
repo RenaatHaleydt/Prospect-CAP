9ac747040693b52a4bcf1870af76d8163e5864f2
Renaat Haleydt <p150480@ap.be>
2024-11-29 12:25:50 +0000

    Trigger Business Process via action

--- srv/services.cds
+++ srv/services.cds
@@ -6,4 +6,11 @@ using { Prospect as my } from '../db/schema';
 service ProspectService { 
     entity Prospects as projection on my.Prospects;
     entity Contacts as projection on my.Persons;
+
+    action TriggerBusinessProcess(Context: BusinessProcessContext) returns String;
+    
+    type BusinessProcessContext {
+		// RoleTitle: String;
+		// RestrictionField: String;
+	}
 }
--- srv/services.js
+++ srv/services.js
@@ -1,32 +1,62 @@
 const httpclient = require("@sap-cloud-sdk/http-client");
 
 module.exports = async function (srv) {
-    srv.before("CREATE", "Prospects", async (oReq) => {
-      console.log("let's try")
-    });
+  srv.before("CREATE", "Prospects", async (oReq) => {
+    console.log("let's try")
+  });
 
-    srv.before("READ", "Prospects", async (oReq) => {
-      let products = await getFirstProductFromNorthwind()
-    });
+  srv.before("READ", "Prospects", async (oReq) => {
+    let products = await getFirstProductFromNorthwind()
+  });
 
+  srv.on("TriggerBusinessProcess", async (oReq) => {
+    let oData = {
+      "definitionId": "us10.44a4fb04trial.prospectprocess.prospectProcess",
+      "context": {
+        "prospect": {
+          "bedrijfsNaam": "cynalco",
+          "email": "infodsfg",
+          "projectNaam": "testings"
+        }
+      }
+    }
+    return await startBusinessProcess(oData)
+  })
 }
 
 async function getFirstProductFromNorthwind() {
-	let oResponse = await httpclient.executeHttpRequest({
-        destinationName: 'northwind'
-    }, {
-        method: 'GET',
-        url: '/Products',
-        headers: {
-            "Content-Type": 'application/json'
-        },
-        params: {
-          top: 1,
-          format: 'json'
-        },
-        parameterEncoder: httpclient.encodeAllParameters
-    }).catch(oError => {
-		  console.log(`Something went wrong connecting to the Northwind destination`)
-    });
-	return oResponse.data;
+  let oResponse = await httpclient.executeHttpRequest({
+    destinationName: 'northwind'
+  }, {
+    method: 'GET',
+    url: '/Products',
+    headers: {
+      "Content-Type": 'application/json'
+    },
+    params: {
+      top: 1,
+      format: 'json'
+    },
+    parameterEncoder: httpclient.encodeAllParameters
+  }).catch(oError => {
+    console.log(`Something went wrong connecting to the Northwind destination`)
+  });
+  return oResponse.data;
+}
+
+async function startBusinessProcess(payload) {
+  let oResponse = await httpclient.executeHttpRequest({
+    destinationName: 'bpmworkflowruntime'
+  }, {
+    method: 'POST',
+    url: '/public/workflow/rest/v1/workflow-instances',
+    headers: {
+      "Content-Type": 'application/json'
+    },
+    data: oData
+  }).catch(oError => {
+    console.log(`Something went wrong connecting to the Build Process Automation destination`)
+    return null
+  });
+  return oResponse.data;
 }
